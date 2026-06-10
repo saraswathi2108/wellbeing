22e9bf830e0539f5eda1c8426ec45727dc85e829
abc <ramyareddy2332@gmail.com>
2026-06-10 18:33:48 +0530

    added

---
+++ src/main/java/com/wellbeing/entity/WellbeingScore.java
@@ -0,0 +1,27 @@
+package com.wellbeing.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+
+public class WellbeingScore {
+
+    @Id
+    private Long wellScoreId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    private Integer currentScore;
+
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "wellbeingScore", cascade = CascadeType.ALL)
+    private List<ScoreHistory> scoreHistories;
+}
