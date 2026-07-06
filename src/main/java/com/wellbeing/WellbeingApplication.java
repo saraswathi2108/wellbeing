f3814aff9d5bcbac997b15813108f6523973e352
Nishanth2244 <thotanishanthnetha222@gmail.com>
2026-07-06 12:11:45 +0530

    Schedular Added

--- src/main/java/com/wellbeing/WellbeingApplication.java
+++ src/main/java/com/wellbeing/WellbeingApplication.java
@@ -2,8 +2,10 @@ package com.wellbeing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,6 +14,11 @@ public class WellbeingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WellbeingApplication.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
---
+++ src/main/java/com/wellbeing/config/KeepAliveScheduler.java
@@ -0,0 +1,29 @@
+package com.wellbeing.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Component
+public class KeepAliveScheduler {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String myUrl = "https://wellbeing-fw2o.onrender.com/api/admin/getConsultations?page=0&size=10";
+
+    @Scheduled(fixedRate = 600000)
+    public void keepAlive() {
+        try {
+            restTemplate.getForObject(myUrl, String.class);
+            System.out.println("Keep-alive ping sent at " + java.time.LocalDateTime.now());
+        } catch (Exception e) {
+            System.err.println("Ping failed: " + e.getMessage());
+        }
+    }
+}   
